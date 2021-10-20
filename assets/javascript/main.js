82262bcf9a2b7fdce5f617e8736e7ad6393829b3
kuldeepp123 <purohitkuldeep91@gmail.com>
2021-10-20 09:53:31 -0700

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+
+
+function login() {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("Signup").style.display = "none";
+    
+}
+function Signup() {
+    document.getElementById("Signup").style.display = "block";
+    document.getElementById("login").style.display = "none";
+    
+}
